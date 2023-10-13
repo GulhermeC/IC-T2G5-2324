b0419f2dbb49d9bd1ed8064ae08fc94c4f80226b
GulhermeC <gjscraveiro@ua.pt>
2023-10-13 16:50:18 +0100

    ex5/ex6 read write txt and bin

--- sndfile-example-src/bitStream.h
+++ sndfile-example-src/bitStream.h
@@ -21,6 +21,7 @@ class BitStream{
         int bitPos;
     
     public:
+        BitStream(){}
         BitStream(string name, string op)
         {
             fName = name;
@@ -34,6 +35,8 @@ class BitStream{
             {
                 file.open(fName, ios::in | ios::binary);
                 fSize = fileSize();
+                int bit = readSingleBit();
+                cout << "out bit:" << bit << "\n";
                 
             }else if(fOperation == "w"){
                 file.open(fName, ios::out | ios::trunc | ios::binary);
@@ -42,7 +45,100 @@ class BitStream{
             }
             
         }
+        void showBits()
+        {
+            for(int i=0;i<bitArray.size();i++)
+                std::cout << bitArray[i] << std::endl;
+        }
+        void ReadTxtFile(string fileName)
+        {
+            std::ifstream file(fileName);
+            std::string line;
 
+            //ficheiro de saida
+            
+            if(file.is_open()) {
+                while (getline(file, line)){
+                    std::cout << line << std::endl;
+                    for(int i=0;i<line.size();i++)
+                    {
+                        bitArray.push_back(line[i]-'0');
+                    }
+
+
+                }
+                file.close();
+            }
+        }
+
+        void WriteTxTFile(string fileName)
+        {
+    
+            //ficheiro de saida
+            std::ofstream fileOut(fileName);
+            if (!fileOut.is_open())
+            {
+                std::cerr << "Erro ao criar ficheiro texto" << std::endl;
+                return;
+            }
+            char byte;
+            if(fileOut.is_open()) 
+            {
+                for(int i=0;i<bitArray.size();i++){
+                    fileOut << bitArray[i];
+                    if ((i+1) % 8 ==0)
+                        fileOut << std::endl;
+                }
+                
+            }
+            fileOut.close();
+        }
+
+        void ReadBinFile(string fileName)
+        {
+            std::ifstream file(fileName,std::ios::binary);
+            char byte;
+            if(file.is_open()) 
+            {
+                while(file.read(&byte,1)){
+                    //gravar no ficheiro de texto
+                    vector<int> bitsConvertidos=byteToBit(byte);
+                    for(int i=0;i<bitsConvertidos.size();i++)
+                        bitArray.push_back(bitsConvertidos[i]-'0');
+                    
+                }
+            }
+            file.close();
+        }
+
+        void WriteBinFile(string fileName)
+        {
+
+            //ficheiro de saida
+            std::ofstream fileOut(fileName,std::ios::binary);
+            if (!fileOut.is_open())
+            {
+                std::cerr << "Erro ao criar ficheiro binário" << std::endl;
+                return;
+            }
+            if(fileOut.is_open()) {
+                vector<int> umbyte;
+                for(int i=0;i<bitArray.size();i++)
+                {
+                    umbyte.push_back(bitArray[i]);
+                    if ((i+1) % 8 ==0){
+                        
+                        char convertido= bitToByte(umbyte);
+                        //std::cout << std::hex << (unsigned int)convertido << std::endl;
+                        fileOut.write(&convertido,1);
+                        umbyte.clear();
+                    }
+
+                }
+                fileOut.close();
+            }
+            
+        }
         int fileSize()
         {
             fstream copy;
@@ -55,15 +151,23 @@ class BitStream{
             copy.seekg(0, copy.end);
             size = copy.tellg();
 
-            cout << "size: " << size << "\n";
+            //cout << "size: " << size << "\n";
             return size;
         }
     //open file
     //fout.open("sample.txt", ios::app);
 
     // read single bit
+        int readSingleBit() {
+            
+        }
 
     // write single bit
+    
+
+        void writeBit(int bit){
+            
+        }
 
     // read N bits (0 <= N <= 64)
 
@@ -73,6 +177,26 @@ class BitStream{
 
     // write string
 
-    // other classes
+// other classes
     
+    vector<int> byteToBit(char byte)
+    {
+        vector<int> bitArr;
+        for (int i = 7; i>= 0; i--)
+        {
+            bitArr.push_back((byte & (1 << i))? '1' : '0');
+        } 
+
+        return bitArr;
+    }
+    // Convert bit array to byte
+    char bitToByte(vector<int> bitArr)
+    {
+        char byte = 0;
+
+        for (int i = 0; i < 8; ++i) {
+            byte |= (bitArr[i] << (7 - i));
+         }      
+        return byte;
+    }
 };
--- sndfile-example-src/encoder.cpp
+++ sndfile-example-src/encoder.cpp
@@ -2,21 +2,128 @@
 #include <vector>
 #include <sndfile.hh>
 #include <math.h>
-#include "bitStream.h";
+#include <fstream>
+#include <string>
+#include "bitStream.h"
+using namespace std;
+
+vector<int> byteToBit(char byte);
+char bitToByte(vector<int> bitArr);
+
+string fileMode;
 
 int 
 main (int argc, char *argv[])
 {
     //input 
-    if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <mode>\n";
+    if(argc < 4) {
+		cerr << "Usage: " << argv[0] << " <input file> <output file> <e|d>\n";
 		return 1;
 	}
 
     string fileName = argv[1];
-    string fileMode = argv[2];
+    string fileNameOut=argv[2];
+    fileMode = argv[3];
+    BitStream bts=BitStream();
 
-    BitStream(fileName, fileMode);
+    if (fileMode=="e")
+    {
+        bts.ReadTxtFile(fileName);
+        bts.showBits();
+        bts.WriteBinFile(fileNameOut);
+    }else{
+        bts.ReadBinFile(fileName);
+        bts.showBits();
+        bts.WriteTxTFile(fileNameOut);
+    }
+
 }
+//TODO CLEAN
+/*
+    if(fileMode=="encode")
+    {
+        //fazer o enconde: txt -> bin
+        std::ifstream file(fileName);
+        std::string line;
+        std::vector<int> bits;
+        //ficheiro de saida
+        std::ofstream fileOut(fileNameOut,std::ios::binary);
+        if (!fileOut.is_open())
+        {
+            std::cerr << "Erro ao criar ficheiro binário" << std::endl;
+            return -1;
+        }
+        if(file.is_open()) {
+            while (getline(file, line)){
+                std::cout << line << std::endl;
+                for(int i=0;i<line.size();i++)
+                {
+                    bits.push_back(line[i]-'0');
+                    //std::cout << bits[i] << std::endl;
+                }
+                char convertido= bitToByte(bits);
+                //gravar o byte convertido
+                fileOut.write(&convertido,1);
+                std::cout << std::hex << (unsigned int)convertido << std::endl;
+                bits.clear();
+            }
+            file.close();
+            fileOut.close();
+        }
+    }else if(fileMode=="decode")//binário para txt
+    {
+        std::ifstream file(fileName,std::ios::binary);
+        std::string line;
+        std::vector<int> bits;
+        //ficheiro de saida
+        std::ofstream fileOut(fileNameOut);
+        if (!fileOut.is_open())
+        {
+            std::cerr << "Erro ao criar ficheiro texto" << std::endl;
+            return -1;
+        }
+        char byte;
+        if(file.is_open()) 
+        {
+            while(file.read(&byte,1)){
+                //gravar no ficheiro de texto
+                vector<int> bitsConvertidos=byteToBit(byte);
+                for(int i=0;i<bitsConvertidos.size();i++)
+                    fileOut << bitsConvertidos[i]-'0';
+                fileOut << std::endl;
+                
+            }
+        }
+        file.close();
+        fileOut.close();
+    }
 
-using namespace std;
+    //BitStream(fileName, fileMode);
+}
+vector<int> byteToBit(char byte)
+    {
+        vector<int> bitArr;
+        for (int i = 7; i>= 0; i--)
+        {
+            bitArr.push_back((byte & (1 << i))? '1' : '0');
+        } 
+
+        return bitArr;
+
+        
+    }
+
+            // Convert bit array to byte
+    char bitToByte(vector<int> bitArr)
+    {
+        char byte = 0;
+
+        for (int i = 0; i < 8; ++i) {
+            if (bitArr[i] != 0 && bitArr[i] != 1) {
+                throw std::invalid_argument("Os elementos do vetor devem ser 0 ou 1.");
+            }
+            byte |= (bitArr[i] << (7 - i));
+         }      
+        return byte;
+    }
+*/
