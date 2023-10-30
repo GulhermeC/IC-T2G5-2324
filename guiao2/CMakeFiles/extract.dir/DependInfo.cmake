f55d6bf0f40623c31c2916abda720b34210b6cf2
GulhermeC <gjscraveiro@ua.pt>
2023-10-30 18:02:08 +0000

    guiao2 makefile

---
+++ guiao2/CMakeFiles/extract.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gjscraveiro/Desktop/Uni/IC/IC-T2G5-2324/guiao2/opencv-src/extract.cpp" "/home/gjscraveiro/Desktop/Uni/IC/IC-T2G5-2324/guiao2/CMakeFiles/extract.dir/extract.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ guiao2/CMakeFiles/extract.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/extract.dir/extract.cpp.o"
+  "opencv-bin/extract"
+  "opencv-bin/extract.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/extract.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
